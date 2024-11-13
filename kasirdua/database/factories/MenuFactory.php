dbcb134f42aa683f61467c72e82b1b9a76c57ea8
kaneaja <kikicuek09@gmail.com>
2024-11-13 16:02:28 +0700

    yang ahmad ahmad aja

---
+++ kasirdua/database/factories/MenuFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Menu>
+ */
+class MenuFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name,
+            'price' => $this->faker->randomNumber(5),
+            'stock' => $this->faker->randomNumber(2),
+            'category_id' => 1,
+        ];
+    }
+}
